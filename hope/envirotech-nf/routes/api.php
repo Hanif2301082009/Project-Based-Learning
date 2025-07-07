e03a77ab5b4ec08be18161d54529479aba85e1be
Hanif al zikri <167852172+Hanif2301082009@users.noreply.github.com>
2025-07-07 11:47:21 +0700

    Delete hope directory

--- hope/envirotech-nf/routes/api.php
+++
@@ -1,4 +0,0 @@
-use App\Http\Controllers\TemperatureController;
-
-Route::post('/temperature', [TemperatureController::class, 'store']);
-Route::get('/temperature', [TemperatureController::class, 'index']);
--- hope/mysql/init/init.sql
+++
@@ -1,4 +0,0 @@
-CREATE DATABASE IF NOT EXISTS dbecommerce_produk;
-CREATE DATABASE IF NOT EXISTS dbecommerce_orders;
-CREATE DATABASE IF NOT EXISTS dbecommerce_payment;
-CREATE DATABASE IF NOT EXISTS dbecommerce_customer;
